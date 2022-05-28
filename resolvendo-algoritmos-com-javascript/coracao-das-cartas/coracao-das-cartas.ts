a9d5d3a305ec94c8ff8ed13e2b3195e75e8872f1
Cai.0 <caioalvesfernandes@gmail.com>
2022-05-28 16:16:28 -0300

    criando solução do desafio 'coracao-das-cartas'

--- resolvendo-algoritmos-com-javascript/coracao-das-cartas/coracao-das-cartas.ts
+++ resolvendo-algoritmos-com-javascript/coracao-das-cartas/coracao-das-cartas.ts
@@ -3,23 +3,12 @@ const coracaoDasCartas = (entry) => {
     let countPilhas = 0;
     while(countPilhas++ < numeroPilhas) {
         const pilha = entry.shift().split(' ').map(i => parseInt(i))
-        let countCartas = 0
-        while(countCartas++ < pilha.length) {
-            const res1 = pilha[countCartas] % 3
-            //Se existir outra carta logo apos
-            let res2 = -1
-            if(pilha[countCartas + 1] !== undefined) {
-                res2 = pilha[countCartas] + pilha[countCartas + 1] % 3
-            }
-            let res3 = -1
-            if(pilha[countCartas + 2] !== undefined) {
-                res3 = pilha[countCartas] + pilha[countCartas + 1] + pilha[countCartas + 2] % 3
-            }
-
-            if(res1 === 0 && res2 === 0 && res3 === 0) {
-                return 0
-            }
-
+        while(pilha.length) {
+            const carta1 = pilha.shift() || 0
+            const carta2 = pilha[1] || 0
+            const carta3 = pilha[2] || 0
+            const somaDosValores = carta1 + carta2 + carta3
+            if(somaDosValores % 3 === 0) return 0
         }
     }
     return 1
